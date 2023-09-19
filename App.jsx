d4d005d1b390e819997aa0127580616e4a598ff4
Maria Bilo <106300843+maria-bilo@users.noreply.github.com>
2023-09-19 11:41:30 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Navigation, Footer, Header, WhatWeDo, Contact } from "./components";
+
+function App() {
+  let languageStoredInLocalStorage = localStorage.getItem("language");
+  let [language, setLanguage] = useState(
+    languageStoredInLocalStorage ? languageStoredInLocalStorage : "English"
+  );
+
+  return (
+    <div className="App">
+      <Navigation
+        language={language}
+        handleSetLanguage={language => {
+          setLanguage(language);
+          storeLanguageInLocalStorage(language);
+        }}
+      />
+      <Header language={language} />
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 mb-5">
+            <WhatWeDo language={language} />
+          </div>
+          <div class="col-md-4 mb-5">
+            <Contact language={language} />
+          </div>
+        </div>
+      </div>
+      <Footer language={language} />
+    </div>
+  );
+}
+
+function storeLanguageInLocalStorage(language) {
+  localStorage.setItem("language", language);
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,5 @@
+export { default as Navigation } from "./components/Navigation";
+export { default as Footer } from "./components/Footer";
+export { default as Header } from "./components/Header";
+export { default as WhatWeDo } from "./components/WhatWeDo";
+export { default as Contact } from "./components/Contact";
